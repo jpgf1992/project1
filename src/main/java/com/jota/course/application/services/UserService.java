5ffdc51bda0e9537982f3d48d72b92b1c6abdc82
Jota <jp.gf1992@gmail.com>
2022-06-08 22:12:14 +0100

    User Delete

--- src/main/java/com/jota/course/application/services/UserService.java
+++ src/main/java/com/jota/course/application/services/UserService.java
@@ -5,6 +5,7 @@ import com.jota.course.application.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -25,5 +26,9 @@ public class UserService {
 
     public Usuario insert(Usuario obj){
        return reposiory.save(obj);
+    }
+
+    public void delete(Long id){
+        reposiory.deleteById(id);
     }
 }
